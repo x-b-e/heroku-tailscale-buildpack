720d35058ee9d2f8cb179ca4cf0f56b80e447fdf
Benjamin Fleischer <github@benjaminfleischer.com>
2025-03-05 20:13:00 -0600

    feat(tailscale): better config variables

--- bin/heroku-tailscale-test.sh
+++ bin/heroku-tailscale-test.sh
@@ -2,21 +2,34 @@
 
 set -e
 
-function log() {
+log_info() {
   echo "-----> $*"
 }
 
-if [ -z "$TAILSCALE_AUTH_KEY" ]; then
-  log "You need to add TAILSCALE_AUTH_KEY to your environment variables."
-else
-  log "Waiting to allow tailscale to finish set up."
-  sleep 10
-  log "Running `tailscale status` You should see your accessible machines on your tailnet."
-  tailscale status
+readonly TAILSCALE_DISABLE="${TAILSCALE_DISABLE:-0}"
+readonly TAILSCALE_DEBUG="${TAILSCALE_DEBUG:-false}"
+case "${TAILSCALE_DEBUG}" in
+  1,true) set -ux ;;
+esac
 
-  log "Running `proxychains4 -f vendor/proxychains-ng/proxychains.conf curl hello.ts.net` "
-  log 'Things are working if you see <a href="https://hello.ts.net">Found</a>.'
-  proxychains4 -f vendor/proxychains-ng/proxychains.conf curl hello.ts.net
-  log "If you didn't see the Found message, then you may need to add the hello.ts.net machine into your tailnet."
-  log "Test complete. I hope you had your fingers crossed!"
+if [ -z "${TAILSCALE_AUTH_KEY:-}" ]; then
+  log_info "You need to add TAILSCALE_AUTH_KEY to your environment variables."
+else
+  case "${TAILSCALE_DISABLE}" in
+    1,true)
+      exit 0
+    ;;
+    *)
+      log_info "Waiting to allow tailscale to finish set up."
+      sleep 10
+      log_info "Running `tailscale status` You should see your accessible machines on your tailnet."
+      tailscale status
+
+      log_info "Running `proxychains4 -f vendor/proxychains-ng/proxychains.conf curl hello.ts.net` "
+      log_info 'Things are working if you see <a href="https://hello.ts.net">Found</a>.'
+      proxychains4 -f vendor/proxychains-ng/proxychains.conf curl hello.ts.net
+      log_info "If you didn't see the Found message, then you may need to add the hello.ts.net machine into your tailnet."
+      log_info "Test complete. I hope you had your fingers crossed!"
+    ;;
+  esac
 fi
